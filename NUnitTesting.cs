d67d45793c24ba7899380e007106d377bd72afc5
Vinay <navik.vinay12@gmail.com>
2022-11-17 21:26:34 +0530

    program to convert celcius/fahrenheit

--- NUnitTesting.cs
+++ NUnitTesting.cs
@@ -6,7 +6,8 @@
         {
             Console.WriteLine("6th Assignment - NUnit Testing !");
             Console.WriteLine("Press 1 : for program on Calculating Monthly Payments on Loan amount\n" +
-                "Press 2 : for calculating Squae root .");
+                "Press 2 : for calculating Square root .\n" +
+                "Press 3 : for Temp Unit Conversion .");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
@@ -15,6 +16,9 @@
                     break;
                 case 2:
                     FindingSqRoot.SquareRoot();
+                    break;
+                case 3:
+                    TempValueConversion.TempUnitConversion();
                     break;
                 default:
                     Console.WriteLine("You missed to enter proper option , Please input only in the given range .");
---
+++ TempValueConversion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thAssignment_NUnitTesting
+{
+    internal class TempValueConversion
+    {
+        public static void TempUnitConversion()
+        {
+            Console.WriteLine("Press 1 : to convert celcius to fahrenheit\n" +
+                              "Press 2 : to convert the fahrenheit to celcius");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Enter a value in Celcuis to convert in Fahrenheit");
+                    double inputInCelcius = Convert.ToDouble(Console.ReadLine());
+                    double outputInFahrenheit = (inputInCelcius * 9 / 5) + 32;
+                    Console.WriteLine($"Input Celcius Value-->{inputInCelcius} and Ouput Fahrenheit value-->{outputInFahrenheit}");
+                    break;
+                case 2:
+                    Console.WriteLine("Enter a value in Fahrenheit to convert in Celcius");
+                    double inputInFahrenheit = Convert.ToDouble(Console.ReadLine());
+                    double outputInCelcius = ((inputInFahrenheit - 32) * 5) / 9;
+                    Console.WriteLine($"Input Fahrenheit Value-->{inputInFahrenheit} and Ouput Celcius value-->{outputInCelcius}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option entered!");
+                    break;
+            }
+        }
+    }
+}
