59961c19bbe1be350cffb68e5322c35c0c36aa39
Vinay <navik.vinay12@gmail.com>
2022-11-17 21:10:33 +0530

    adding program to calculate monthy EMI's on Loan

---
+++ MonthlyPayment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thAssignment_NUnitTesting
+{
+    internal class MonthlyPayment
+    {
+        public static void CalculatingMonthlyPayment()
+        {
+            
+            Console.WriteLine("Calculating the payments to be done on monthly basis with regards to the loan taken from someone");
+            //taking inputs on P,Y and R (Principal amount ,Year/Duration , Rate of Interest (ROI)
+            Console.WriteLine("Please enter the loan amount you wish to have");     
+            double P = Convert.ToDouble(Console.ReadLine());    
+            Console.WriteLine("Enter the duration of Loan Tenure in years (loan for how many years?)");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Rate of Interest will be : ");
+            double R = Convert.ToDouble(Console.ReadLine());
+            //divided the formula into small parts to get the same calculated .
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+            double upsideCalculation = P * r;
+            double downsideAddition = 1 + r;
+            double downsideRaisedToCalculation = Math.Pow(downsideAddition, -n);
+            double downsideCalculation = 1 - downsideRaisedToCalculation;
+            double monthlyPayment = upsideCalculation / downsideCalculation;
+            Console.WriteLine($"Monthly Payment will be : {monthlyPayment}");   //output.
+        }
+    }
+}
---
+++ NUnitTesting.cs
@@ -0,0 +1,22 @@
+﻿namespace _6thAssignment_NUnitTesting
+{
+    internal class NUnitTesting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("6th Assignment - NUnit Testing !");
+            Console.WriteLine("Press 1 : for program on Calculating Monthly Payments on Loan amount");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    MonthlyPayment.CalculatingMonthlyPayment();
+                    break;
+                default:
+                    Console.WriteLine("You missed to enter proper option , Please input only in the given range .");
+                    break;
+            }
+            
+        }
+    }
+}
