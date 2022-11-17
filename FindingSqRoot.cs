f616618298d1d6ffcdb2ae6a0d872fde70112033
Vinay <navik.vinay12@gmail.com>
2022-11-17 21:20:16 +0530

    newtons formula to find sq root

---
+++ FindingSqRoot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6thAssignment_NUnitTesting
+{
+    internal class FindingSqRoot
+    {
+        public static void SquareRoot()
+        {
+            Console.WriteLine("enter a number to find its square root ");
+            double c = Convert.ToDouble(Console.ReadLine());    //taking a number to calculate Sq.root.
+            Console.Write($"Square root of {c} is :");
+            double t = c;
+            double epsilon = 1.0e-15;                   //1.0e-15 = 1.0 x 10^-15 = 0.0000000000000010
+
+            while (Math.Abs(t - c / t) > epsilon * t)   //Math.Abs()--> absolute positive no .
+            {
+                t = (c / t + t) / 2.0;                  //pre-defined formula 
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
